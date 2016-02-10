ad35299b901ad93df80570145e4f566054ea308b
ytakzk <shyss.ak@gmail.com>
2016-02-11 00:18:06 +0900

    Initial Commit

---
+++ Swift-Simple-Interactive-Art/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Simple-Interactive-Art
+//
+//  Created by Yuta Akizuki on 2016/02/11.
+//  Copyright © 2016年 ytakzk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
